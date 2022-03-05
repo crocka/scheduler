bf197435262cbd253f713bc8697db038aac53923
crocka <chino123anc@hotmail.com>
2022-03-04 20:41:11 -0500

    Debug and fix errors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,16 @@
 export function getAppointmentsForDay(state, day) {
 
+  //find the selected day
   const selectedDay = state.days.filter(sday => sday.name === day);
 
+  // if the selected day does not exist
   if (selectedDay.length === 0) {
 
     return [];
 
   } else {
 
+    //return an array of appointment object of the day
     return selectedDay[0].appointments.map(num => state.appointments[num]);
 
   };
@@ -19,10 +22,13 @@ export function getInterview(state, interview) {
 
   let interviewInfo = null;
 
+  //if interview is not null
   if (interview) {
 
+    //copy the interview to a new object
     interviewInfo = {...interview};
 
+    //insert the interviewer object to interviewInfo
     interviewInfo.interviewer = state.interviewers[interview.interviewer];
 
   }
@@ -33,14 +39,17 @@ export function getInterview(state, interview) {
 
 export function getInterviewersForDay(state, day) {
 
+  //find the object of the selected day
   const selectedDay = state.days.filter(sday => sday.name === day);
 
+  // if the selected day does not exist
   if (selectedDay.length === 0) {
 
     return [];
 
   } else {
 
+    //return an array of interviewers specific to that day
     return selectedDay[0].interviewers.map(num => state.interviewers[num]);
 
   };
